0fe58d86a75d7d2e8c215db604036632a7131d92
Cameron <cedenni2@ncsu.edu>
2024-07-07 14:37:29 -0400

    basic board is functional, can print

--- sudoku.py
+++ sudoku.py
@@ -4,6 +4,7 @@
 import array as arr
 
 GRID_SIZE = 9
+TOTAL_SQUARES = GRID_SIZE ** 2
 
 class Notes(object):
     """Class for notes on squares within grid
@@ -25,7 +26,7 @@ Notes are stored on a single variable using bitmasking
             return True
         return False
 
-    def toggle(self, value):
+    def toggle(self, value: int):
         if self.check(value):
             self.clear(value)
         else: 
@@ -43,15 +44,41 @@ Notes are stored on a single variable using bitmasking
 class Square(object):
     """Class for squares within grid"""
     
-    def __init__(self):
-        self.value = 0
+    def __init__(self, value: int = 0 ):
+        self.value = int(value)
+        self.notes = Notes()
+
+    def val(self):
+        return self.value
         pass
 
 class Sudoku(object):
     """Class for Sudoku"""
 
-    def __init__(self):
+    dim = GRID_SIZE
+    
+    def __init__(self, values = []):
+        if (type(values) is list) and (len(values) == TOTAL_SQUARES):
+            self.board = [ Square(value) for value in values ]
+        else:
+            self.board = [ Square() ] * (self.dim ** 2)
+
         pass
+
+    def view(self):
+        output = ""
+        for val_ind in range(0, TOTAL_SQUARES):
+            output += str(self.board[val_ind].val())
+            output += " "
+
+            if (not (val_ind + 1)%3) and ((val_ind + 1) % GRID_SIZE):
+                output += "| "
+            if not (val_ind + 1)%GRID_SIZE:
+                output += "\n"
+            if not (val_ind + 1)%(GRID_SIZE*3) and ((val_ind + 1) % GRID_SIZE**2):
+                output += "-" * (GRID_SIZE * 2 + 3)
+                output += "\n"
+        print(output)
 
     pass
 
--- test_dev.py
+++ test_dev.py
@@ -1,25 +1,38 @@
 """Testing!
 """
 
-from sudoku import Notes
+from sudoku import Notes, Sudoku
 
-a = Notes()
-a.mark(1)
-a.mark(2)
-a.mark(9)
+def notes_basic():
+    a = Notes()
+    a.mark(1)
+    a.mark(2)
+    a.mark(9)
 
-a.clear(8)
-a.clear(2)
+    a.clear(8)
+    a.clear(2)
 
-a.view()
+    a.view()
 
-print(a.check(1))
-print(a.check(2))
+    print(a.check(1))
+    print(a.check(2))
 
-a.toggle(1)
-a.toggle(2)
+    a.toggle(1)
+    a.toggle(2)
 
-a.toggle(5)
-a.toggle(5)
+    a.toggle(5)
+    a.toggle(5)
 
-a.view()
+    a.view()
+
+
+def board_basic():
+    board_0 = Sudoku()
+    board_0.view()
+    with open("easy50.txt") as board_file:
+        board_1 = list(board_file.readline())[:-1]
+        board = Sudoku(board_1)
+        board.view()
+
+
+board_basic()
